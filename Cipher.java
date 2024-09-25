7bb72ceda15121e8807891964349eeafcafa0588
Yavuz Selim Kahyaoğlu <yavuzskhy@gmail.com>
2024-09-25 14:41:19 +0300

    Fixed  errors
    I fixed errors in order to run the program properly

--- Cipher.java
+++ Cipher.java
@@ -15,7 +15,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            outputString += replaceChar(inputString.charAt(i),true);
         }
 
         return outputString;
@@ -26,7 +26,12 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
-        replaceChar('a',true);
+        // for all chars in the input string
+        for (int i = 0; i < inputString.length(); i++)   
+        {
+            outputString += replaceChar(inputString.charAt(i),false);
+        }
+        
         
         return outputString;
     }
@@ -42,7 +47,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
